d72c3fc66caa4d559d895b2a97882537f068adde
Max Korenkov <mkorenkov@gmail.com>
2011-05-16 16:21:44 +0300

    copying soyutils.js

--- tools/pre-build.sh
+++ tools/pre-build.sh
@@ -1,3 +1,4 @@
 #!/bin/sh
 
+cp ./closure-templates-for-javascript-latest/soyutils.js ../scripts/
 java -jar closure-templates-for-javascript-latest/SoyToJsSrcCompiler.jar  --outputPathFormat ../scripts/index.js ../templates/index.soy
