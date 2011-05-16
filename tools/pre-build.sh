8294da8fd011f91120a18e9a94a9221f09f5febf
Max Korenkov <mkorenkov@gmail.com>
2011-05-16 21:51:09 +0300

    Splitted template files

--- tools/pre-build.sh
+++ tools/pre-build.sh
@@ -1,4 +1,8 @@
 #!/bin/sh
 
 cp ./closure-templates-for-javascript-latest/soyutils.js ../scripts/
+java -jar closure-templates-for-javascript-latest/SoyToJsSrcCompiler.jar  --outputPathFormat ../scripts/about.js ../templates/about.soy
+java -jar closure-templates-for-javascript-latest/SoyToJsSrcCompiler.jar  --outputPathFormat ../scripts/contacts.js ../templates/contacts.soy
+java -jar closure-templates-for-javascript-latest/SoyToJsSrcCompiler.jar  --outputPathFormat ../scripts/news.js ../templates/news.soy
+java -jar closure-templates-for-javascript-latest/SoyToJsSrcCompiler.jar  --outputPathFormat ../scripts/register.js ../templates/register.soy
 java -jar closure-templates-for-javascript-latest/SoyToJsSrcCompiler.jar  --outputPathFormat ../scripts/index.js ../templates/index.soy
